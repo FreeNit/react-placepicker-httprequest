e9c8a035b7acf71651043ad4a1b8714882b04d61
Dimon <bolilui.dmytro@gmail.com>
2024-01-30 21:12:08 +0200

    Added useEffect to send HTTP requests

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,11 +1,33 @@
+import { useState, useEffect } from 'react';
+
+// * Import Components
 import Places from './Places.jsx';
 
+const places = localStorage.getItem('places');
+
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
+      title='Available Places'
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText={'Fetching place data...'}
+      fallbackText='No places available.'
       onSelectPlace={onSelectPlace}
     />
   );
